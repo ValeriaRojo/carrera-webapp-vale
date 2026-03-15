0b1c0629b60fef6d7fffd134bf422891a96cf62a
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-14 22:16:47 -0600

    Configuración de screens: login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginScreen } from './screens/login-screen/login-screen';
+import { RegistroScreen } from './screens/registro-screen/registro-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginScreen, pathMatch: 'full'},
+    {path: 'registro', component: RegistroScreen, pathMatch: 'full'},
+];
---
+++ src/app/screens/registro-screen/registro-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-screen',
+  imports: [],
+  templateUrl: './registro-screen.html',
+  styleUrl: './registro-screen.scss',
+})
+export class RegistroScreen {
+
+}
