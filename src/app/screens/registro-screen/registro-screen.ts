4fbc5a948435918462cb100e85b14b35f19dd499
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-15 16:06:27 -0600

    Ngx instalado y registro-screen.ts editado

--- src/app/screens/registro-screen/registro-screen.ts
+++ src/app/screens/registro-screen/registro-screen.ts
@@ -10,7 +10,18 @@ import { SHARED_IMPORTS } from '../../shared/shared.imports';
   styleUrl: './registro-screen.scss',
 })
 export class RegistroScreen implements OnInit {
+
+  public user!: RegistroUser;
+  public errors: RegistroErrors = {};
+  public isLoading = false;
+
+  public hide_1 = true;
+  public inputType_1: 'password' | 'text' = 'password';
+
+  public edades: Array<{ value: number }> = [];
+
   
+
   constructor() { }
 
   ngOnInit(): void {
