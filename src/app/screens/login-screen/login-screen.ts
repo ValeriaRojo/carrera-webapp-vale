fee87e5a1bdbceae8eb2153c4d6b5cc6ce900ba2
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-14 22:28:28 -0600

    Carpeta de importaciones configurada

--- src/app/screens/login-screen/login-screen.ts
+++ src/app/screens/login-screen/login-screen.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [
+    ...SHARED_IMPORTS,
+  ],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.scss',
 })
