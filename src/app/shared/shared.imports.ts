972b5efea1b9ecbb1b303f2f549882406d0f3410
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-14 23:11:03 -0600

    Esqueleto de login

--- src/app/shared/shared.imports.ts
+++ src/app/shared/shared.imports.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 export const SHARED_IMPORTS = [
     CommonModule,
     FormsModule,
     MatButtonModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
 ] as const;
