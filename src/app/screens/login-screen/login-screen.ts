a32a9ef7b04a7c006cfacdbc9db720fb2959fa97
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-14 23:26:20 -0600

    Funciones y variables implementadas en el typescript de login

--- src/app/screens/login-screen/login-screen.ts
+++ src/app/screens/login-screen/login-screen.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
@@ -10,6 +11,44 @@ import { SHARED_IMPORTS } from '../../shared/shared.imports';
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.scss',
 })
-export class LoginScreen {
+export class LoginScreen implements OnInit {
+
+  public username: string = "";
+  public password: string = "";
+  public errors:any = {};
+  //Para la contraseña
+  public hide_1: boolean = false;
+  public inputType_1: string = 'password';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  public showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  public login(){
+    this.router.navigate(['app', "home"]);
+  }
+
+  public recuperarPwd(){
+
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro"]);
+  }
 
 }
